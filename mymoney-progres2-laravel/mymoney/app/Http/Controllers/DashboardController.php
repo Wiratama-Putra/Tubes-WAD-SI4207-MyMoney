39345fb606d10c53e61e8e39e6577925d7bed8f8
nabilacindyputri <nabilacindyputri@gmail.com>
2020-12-22 18:41:24 +0700

    membuat API

--- mymoney-progres2-laravel/mymoney/app/Http/Controllers/DashboardController.php
+++ mymoney-progres2-laravel/mymoney/app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\belanjaRequest;
 use App\Models\Note;
 use App\Models\Transaction;
 use App\Models\User;
@@ -125,6 +126,58 @@ class DashboardController extends Controller
     {
         return view('pages.user', [
             'user' => Auth::user()
+        ]);
+    }
+
+    public function belanjaApi(belanjaRequest $request)
+    {
+        $balance = (User::findOrFail($request->id)->only('balance'))['balance'];
+        $point = (User::findOrFail($request->id)->only('point'))['point'];
+
+
+
+        User::where('id', $request->id)->update([
+            'balance' => $balance - (int)$request->nominal,
+            'point' => $point + 10
+        ]);
+        Transaction::create([
+            'deskripsi' => 'Belanja-' . $request->bank . '-' . $request->vendor,
+            'nominal' => (int)$request->nominal,
+            'user_id' => $request->id
+        ]);
+        return response()->json([
+            'status' => 'berhasil',
+            'data_transaksi' => [
+                'bank' => $request->bank,
+                'nominal' => $request->nominal,
+                'vendor' => $request->vendor
+            ],
+            'data_pengguna' => [
+                'id' => $request->id,
+                'nama' => User::findOrFail($request->id)->name,
+                'saldo' => User::findOrFail($request->id)->balance,
+                'poin' => User::findOrFail($request->id)->point,
+            ]
+        ]);
+        // return response()->json([
+        //     'data' => 'berhasil',
+        //     'status' => [
+        //         'judul' => $request->judul,
+        //         'harga' => $request->harga,
+        //     ]
+        // ]);
+    }
+    public function ceksaldoApi(Request $request)
+    {
+        $request->validate([
+            'id' => 'required'
+        ]);
+        $user = User::findOrFail($request->id);
+        return response()->json([
+            'id' => $user->id,
+            'name' => $user->name,
+            'saldo' => $user->balance,
+            'poin' => $user->point,
         ]);
     }
 }
